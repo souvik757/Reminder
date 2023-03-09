ee219227c6eb7faaede725e6f68f7bcfc8cd25de
souvik757 <88132246+souvik757@users.noreply.github.com>
2023-03-09 09:40:53 +0530

    -02- commit -- push

---
+++ app/src/main/java/com/example/reminder/NotesDetails.java
@@ -0,0 +1,14 @@
+package com.example.reminder;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class NotesDetails extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_notes_details);
+    }
+}
